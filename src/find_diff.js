b2e7ce7b0535da38a77eb696038a7025e5ddf2b6
SupriyaGoleTW <supriyag@thoughtworks.com>
2017-05-15 19:11:23 +0530

    Wrote first working alogorithm to find out spelling mistakes and missing words

--- src/find_diff.js
+++ src/find_diff.js
@@ -1,29 +1,30 @@
-var getDiff = function () {
-    console.log("hello");
-    var other = 'good boy',
-        color = '',
-        span = null;
+var getDiff1 = function () {
+    var other = 'Thank you to all my wonderful teachers, who inspired me to believe in myself and be brave.I am proud, well in fact, I am very proud to be the first Pashtun, the first Pakistani, and the youngest person to receive this award. Along with that, along with that, I am pretty certain that I am also the first recipient of the Nobel Peace Prize who still fights with her younger brothers. I want there to be peace everywhere, but my brothers and I are still working on that.';
+    var diff = JsDiff.diffWords(other, document.getElementById('para').value);
+    var diffData = diff.map(function(each){
+        if(each.added){
+            each.color = "red"
+        }else if(each.removed){
+            each.color = "orange"
+        }else{
+            each.color = "black"
+        }
+        return each;
 
-    var diff = JsDiff.diffWords(document.getElementById('userText').value, other),
-        display = document.getElementById('display');
-        fragment = document.createDocumentFragment();
-
-
-    diff.forEach(function (part) {
-        console.log("blalll");
-        alert("bla")
-        // green for additions, red for deletions
-        // grey for common parts
-
-        color = part.added ? 'blue' :
-            part.removed ? 'red' : 'black';
-        span = document.createElement('span');
-        console.log("span ",span);
-        span.style.color = color;
-        span.appendChild(document
-            .createTextNode(part.value));
-        fragment.appendChild(span);
     });
+    
+    var htmlData = diffData.map(function (part,index) {
+        if(diffData.length > 1 && index < diffData.length - 1 && diffData[index + 1].color == "red" && diffData[index].color == "orange")
+            return;
+        else {
+            var word = "<span style='color: " + part.color + "'>";
+            if (part.color == "orange")
+                word += " __";
+            else
+                word += part.value;
+            return word += "</span>";
+        }
+    }).join("");
 
-    display.appendChild(fragment);
+    document.getElementById("difference").innerHTML = htmlData;
 };
