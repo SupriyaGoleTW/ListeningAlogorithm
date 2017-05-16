aa0dc91380aebe16fafa3a31e6d80b5d503a4a63
SupriyaGoleTW <supriyag@thoughtworks.com>
2017-05-16 19:48:27 +0530

    Basic algorithm done | <Supriya/Sanjeet>

--- src/find_diff.js
+++ src/find_diff.js
@@ -1,25 +1,30 @@
 var getDiff1 = function () {
-    var other = 'Thank you to all my wonderful teachers, who inspired me to believe in myself and be brave.I am proud, well in fact, I am very proud to be the first Pashtun, the first Pakistani, and the youngest person to receive this award. Along with that, along with that, I am pretty certain that I am also the first recipient of the Nobel Peace Prize who still fights with her younger brothers. I want there to be peace everywhere, but my brothers and I are still working on that.';
+    var other = 'Thank you to all my wonderful teachers';
     var diff = JsDiff.diffWords(other, document.getElementById('para').value);
-    var diffData = diff.map(function(each){
-        if(each.added){
+    var diffData = diff.map(function (each) {
+        if (each.added) {
             each.color = "red"
-        }else if(each.removed){
+        } else if (each.removed) {
             each.color = "orange"
-        }else{
+        } else {
             each.color = "black"
         }
         return each;
-
     });
-    
-    var htmlData = diffData.map(function (part,index) {
-        if(diffData.length > 1 && index < diffData.length - 1 && diffData[index + 1].color == "red" && diffData[index].color == "orange")
+
+    diffData.forEach(function (eachDiff,index) {
+        if (diffData.length > 1 && index < diffData.length - 1 && diffData[index + 1].color == "red" && eachDiff.color == "black") {
+            diffData[index + 1].color = "blue";
+        }
+    });
+
+    var htmlData = diffData.map(function (part, index) {
+        if (diffData.length > 1 && index < diffData.length - 1 && diffData[index + 1].color == "red" && diffData[index].color == "orange")
             return;
         else {
             var word = "<span style='color: " + part.color + "'>";
             if (part.color == "orange")
-                word += " __";
+                word += " __ ";
             else
                 word += part.value;
             return word += "</span>";
@@ -28,3 +33,5 @@ var getDiff1 = function () {
 
     document.getElementById("difference").innerHTML = htmlData;
 };
+
+// module.exports= getDiff1;
